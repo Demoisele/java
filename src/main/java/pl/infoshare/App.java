40cd061da6a89c276241e2438d4990010b5899b4
Ada Stajewska <demoiselle80@gmail.com>
2018-10-10 17:31:04 +0200

    Initial commit

---
+++ src/main/java/pl/infoshare/App.java
@@ -0,0 +1,17 @@
+package pl.infoshare;
+
+import java.sql.SQLOutput;
+
+/**
+ * Ada Stajewska
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {byte age = 38;
+    String hometown = "Gdynia";
+        System.out.println( age + "\t" + hometown);
+
+    }
+}
