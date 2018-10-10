b1f264dad7ca7462e065db4639dbe1b8ae6bc777
Ada Stajewska <demoiselle80@gmail.com>
2018-10-10 20:11:59 +0200

    w

--- src/main/java/pl/infoshare/App.java
+++ src/main/java/pl/infoshare/App.java
@@ -1,17 +1,20 @@
 package pl.infoshare;
 
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 /**
  * Ada Stajewska
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {byte age = 38;
-    String hometown = "Gdynia";
-        System.out.println( age + "\t" + hometown);
+    public static void main( String[] args ){
+    Scanner scanner = new Scanner(System.in);
+    System.out.print(" Podaj liczbe calkowita :");
+    double number = scanner.nextDouble();
+    System.out.println("Podales liczbe :" + number);
+
 
     }
 }
